9f693bae4c7bab97ffaa8db4f7f6e816a9a5dad6
Your Name <you@example.com>
2025-09-30 19:33:08 +0300

    update to use backend

--- pasos/macos/Flutter/GeneratedPluginRegistrant.swift
+++ pasos/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,6 +6,7 @@ import FlutterMacOS
 import Foundation
 
 import file_picker
+import flutter_secure_storage_macos
 import path_provider_foundation
 import sqflite_darwin
 import url_launcher_macos
@@ -13,6 +14,7 @@ import video_player_avfoundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
+  FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
